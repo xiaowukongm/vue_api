a2aa3a9e2ac01a532cc645d163615c9668e75f75
xiaowukong <2363497179@qq.com>
2020-03-31 00:07:55 +0800

    构建菜单数据

--- data/views.py
+++ data/views.py
@@ -18,18 +18,79 @@ def login(request):
 @require_GET
 def get_menus(request):
     # 菜单数据结构
-    data = {
-        "id":101,
-        "authName":"商品管理",
-        "path":"",
-        "children":[
+    userMenu = {
+        "id": 101,
+        "authName": "用户管理",
+        "path": "",
+        "children": [
             {
-                "id": 104,
-                "authName": "商品列表",
-                "path": "",
-                "children":[],
+                "id": 101-1,
+                "authName": "用户列表",
+                "path": "users",
+                "children": [],
             }
         ]
     }
-    result = {"data":[data],"msg":"获取菜单成功！"}
+
+    permissionMenu =  {
+        "id":102,
+        "authName":"权限管理",
+        "path":"",
+        "children":[
+            {
+                "id": 102-1,
+                "authName": "角色列表",
+                "path": "role",
+                "children":[],
+            },
+            {
+                "id": 102 - 2,
+                "authName": "权限列表",
+                "path": "permission",
+                "children": [],
+            }
+
+        ]
+    }
+    shopMenu = {
+        "id": 103,
+        "authName": "商品管理",
+        "path": "",
+        "children": [
+            {
+                "id": 103-1,
+                "authName": "商品列表",
+                "path": "shop",
+                "children": [],
+            }
+        ]
+    }
+    orderMenu = {
+        "id": 104,
+        "authName": "订单管理",
+        "path": "",
+        "children": [
+            {
+                "id": 104-1,
+                "authName": "订单列表",
+                "path": "order",
+                "children": [],
+            }
+        ]
+    }
+    dataMenu = {
+        "id": 105,
+        "authName": "数据统计",
+        "path": "",
+        "children": [
+            {
+                "id": 105-1,
+                "authName": "数据图表",
+                "path": "data",
+                "children": [],
+            }
+        ]
+    }
+    data = [userMenu,permissionMenu,shopMenu,orderMenu,dataMenu]
+    result = {"data":data,"msg":"获取菜单成功！"}
     return HttpResponse(json.dumps(result))
